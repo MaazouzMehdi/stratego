19c4a53fb9c2dd259fe8c0888ffb9d9fe576d658
Marco <150938@student.umons.ac.be>
2015-05-12 22:11:59 +0200

    creation cell*, river

---
+++ src/be/ac/umons/stratego/pion/River.java
@@ -0,0 +1,21 @@
+package be.ac.umons.stratego.pion;
+
+import be.ac.umons.stratego.plateau.PlateauBase;
+
+/**
+ * Created by marco on 9/05/15.
+ */
+public class River {
+    int posY;
+    int posX;
+
+    public River(int posY, int posX){
+        this.posY=posY;
+        this.posX=posX;
+    }
+
+    @Override
+    public String toString() {
+        return "River";
+    }
+}
