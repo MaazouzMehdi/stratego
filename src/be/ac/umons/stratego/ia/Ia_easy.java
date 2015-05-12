a8358484548545c2d8df47fe4c6fb450c8cf9e71
Marco <150938@student.umons.ac.be>
2015-05-12 22:00:45 +0200

    IA_easy

--- src/be/ac/umons/stratego/ia/Ia_easy.java
+++ src/be/ac/umons/stratego/ia/Ia_easy.java
@@ -1,102 +1,94 @@
 package be.ac.umons.stratego.ia;
 
+import be.ac.umons.stratego.pion.Cell;
+import be.ac.umons.stratego.pion.Direction;
 import be.ac.umons.stratego.pion.Pion;
 import be.ac.umons.stratego.plateau.PlateauBase;
 
-import java.lang.String;
 import java.util.ArrayList;
 
 
 import java.util.Random;
 
-import static be.ac.umons.stratego.pion.Pion.conversion;
 
 
 public class Ia_easy {
 
-    private ArrayList<Pion> piondispo ;
-
-        public void play (PlateauBase plateau) {
-
+    public static ArrayList<Pion> pawnsDisponible = new ArrayList<>(40); // VOIR SYNTAXE
+    public static ArrayList<Pion> pawnsCanMove = new ArrayList<>(40);
 
 
-            Pion pion = choice(plateau);// truc qui choisis le string pion de liste dispo
-            Random myrandomdeplacement = new Random();
-            int deplacement = myrandomdeplacement.nextInt(3);
+    public static void play(PlateauBase plateau) {
 
-            if (pion.toString().equals("Sc-E")) {
+
+        Pion pion = choice(plateau);// truc qui choisis le pion de liste dispo
+        Random myrandomdeplacement = new Random();
+        int direction = myrandomdeplacement.nextInt(4);
+
+            if (pion.toString().equals("Sc")) {
 
                 Random mynumbercases = new Random();
                 int nbrcases = mynumbercases.nextInt(5);
-
-                /*if (deplacement == 0) {
-                    if (pion.deplacementPossible("UP", plateau, nbrcases))
-                        pion.deplacement("UP", plateau, nbrcases);
-                    else
-                        play(plateau); // ainsi on est sur qu'il se deplace bien
-                } else if (deplacement == 2) {
-                    if (pion.deplacementPossible("DOWN", plateau, nbrcases))
-                        pion.deplacement("DOWN", plateau, nbrcases);
-                    else
-                        play(plateau);
-                } else if (deplacement == 1) {
-                    if (pion.deplacementPossible("LEFT", plateau, nbrcases))
-                        pion.deplacement("LEFT", plateau, nbrcases);
-                    else
-                        play(plateau);
+                if (direction == 0) {
+                    if (pion.deplacementPossible(Direction.NORTH, plateau, nbrcases))
+                        pion.deplacement(Direction.NORTH, plateau, nbrcases);
+                } else if (direction == 2) {
+                    if (pion.deplacementPossible(Direction.SOUTH, plateau, nbrcases))
+                        pion.deplacement(Direction.SOUTH, plateau, nbrcases);
+                } else if (direction == 1) {
+                    if (pion.deplacementPossible(Direction.WEST, plateau, nbrcases))
+                        pion.deplacement(Direction.WEST, plateau, nbrcases);
                 } else {
-                    if (pion.deplacementPossible("RIGHT", plateau, nbrcases))
-                        pion.deplacement("RIGHT", plateau, nbrcases);
-                    else
-                        play(plateau);
+                    if (pion.deplacementPossible(Direction.EAST, plateau, nbrcases))
+                        pion.deplacement(Direction.EAST, plateau, nbrcases);
                 }
-                } else if (deplacement == 0) {
-                    if (pion.deplacementPossible("UP", plateau))
-                        pion.deplacement("UP", plateau);
-                    else
-                        play(plateau); // ainsi on est sur qu'il se deplace bien
-                } else if (deplacement == 2) {
-                    if (pion.deplacementPossible("DOWN", plateau))
-                        pion.deplacement("DOWN", plateau);
-                    else
-                        play(plateau);
-                } else if (deplacement == 1) {
-                    if (pion.deplacementPossible("LEFT", plateau))
-                        pion.deplacement("LEFT", plateau);
-                    else
-                        play(plateau);
+            }
+            else {
+
+                if (direction == 0) {
+                    if (pion.deplacementPossible(Direction.NORTH, plateau,1))
+                        pion.deplacement(Direction.NORTH, plateau,1);
+                } else if (direction == 2) {
+                    if (pion.deplacementPossible(Direction.SOUTH, plateau,1))
+                        pion.deplacement(Direction.SOUTH, plateau,1);
+                } else if (direction == 1) {
+                    if (pion.deplacementPossible(Direction.WEST, plateau,1))
+                        pion.deplacement(Direction.WEST, plateau,1);
                 } else {
-                    if (pion.deplacementPossible("RIGHT", plateau))
-                        pion.deplacement("RIGHT", plateau);
-                    else
-                        play(plateau);
-            }*/
+                    if (pion.deplacementPossible(Direction.EAST, plateau,1))
+                        pion.deplacement(Direction.EAST, plateau,1);
+                }
             }
         }
 
-        public Pion choice (PlateauBase plateau) { // pour le momment on fait la supposition qu'il possède toujours des pions
-            generatePossibilties(plateau); // pas très grande complexité je sais mais pas lent si ?
+        private static Pion choice(PlateauBase plateau) { // pour le momment on fait la supposition qu'il possède toujours des pions, victory gère ca
+            generatePossibilties(plateau);
             Random choicepion = new Random();
-            int myindex = choicepion.nextInt(piondispo.size());
-            Pion mychoice = piondispo.get(myindex);
+            int myindex = choicepion.nextInt(pawnsCanMove.size());
+            Pion mychoice = pawnsCanMove.get(myindex);
             return mychoice;
 
         }
 
-        public void generatePossibilties(PlateauBase plateau ){
+        private static void generatePossibilties(PlateauBase plateau){
 
             for (int i=0;i<10;i++) {
                 for (int j = 0; j < 10; j++) {
-                    String ipion = plateau.zone[i][j];
-                    if (!ipion.equals("null") && !ipion.equals("FLEUVE")) {
-                        Pion monpion = conversion(ipion, i, j);
+                    Cell cellpion = plateau.board[i][j];
+                    if (cellpion!=null && cellpion.getThisriverpiece()==null) {
+                        Pion monpion = cellpion.getThispiece();
                         if (monpion.getSquad().equals("Ennemy")) {
-                            piondispo.add(monpion);
-
+                            pawnsDisponible.add(monpion);
                         }
                     }
-
                 }
+            }
+            for (Pion pawns:pawnsDisponible) {
+                if (pawns.deplacementPossible(Direction.NORTH,plateau,1)
+                        ||pawns.deplacementPossible(Direction.SOUTH,plateau,1)
+                        ||pawns.deplacementPossible(Direction.WEST,plateau,1)
+                        ||pawns.deplacementPossible(Direction.EAST,plateau,1) )
+                    pawnsCanMove.add(pawns);
             }
         }
 }
