5837331fafad176191a44d4bd196eae8dea68b21
Mehdi <150417@student.umons.ac.be>
2015-05-13 12:14:44 +0200

    cell oublié

---
+++ src/be/ac/umons/stratego/pion/Cell.java
@@ -0,0 +1,72 @@
+package be.ac.umons.stratego.pion;
+
+/**
+ * Created by marco on 6/05/15.
+ */
+public class Cell {
+
+    private Pion thispiece;
+    private River thisriverpiece;
+
+    public Cell( CellObject pioncase, int posY , int posX , String squad) {
+    
+        switch (pioncase) {
+            case BOMB:
+                thispiece = new Bomb(posY, posX, squad);
+                break;
+            case SCOUT:
+                thispiece = new Scout( posY, posX, squad);
+                break;
+            case CAPTAIN:
+                thispiece = new Captain( posY, posX, squad);
+                break;
+            case SPY:
+                thispiece = new Spy(posY, posX, squad);
+                break;
+            case SERGEANT:
+                thispiece = new Sergeant(posY, posX, squad);
+                break;
+            case MINER:
+                thispiece = new Miner(posY, posX, squad);
+                break;
+            case MARECHAL:
+                thispiece = new Marechal(posY, posX, squad);
+                break;
+            case MAJOR:
+                thispiece = new Major(posY, posX, squad);
+                break;
+            case LIEUTENANT:
+                thispiece = new Lieutenant(posY, posX, squad);
+                break;
+            case GENERAL:
+                thispiece = new General(posY, posX, squad);
+                break;
+            case FLAG:
+                thispiece = new Flag( posY, posX, squad);
+                break;
+            case COLONEL:
+                thispiece = new Colonel(posY, posX, squad);
+                break;
+            case RIVER:
+            	thisriverpiece= new River(posY,posX);
+            	
+        }
+    }
+
+    public Cell(CellObject river, int posY , int posX){
+        thisriverpiece = new River(posY, posX);
+    }
+
+    public Pion getThispiece() {
+        return thispiece;
+    }
+    
+    public void setThispiece(Pion thispiece) {
+    	this.thispiece=thispiece;
+    }
+    
+    public River getThisriverpiece() {
+        return thisriverpiece;
+    }
+    
+}
