a980ab976a3b4f062110cef254fdc45ae548775e
Marco <150938@student.umons.ac.be>
2015-05-15 04:12:34 +0200

    IA finie + problèmes réglés yessssssssssssssss

--- src/be/ac/umons/stratego/ia/Ia_Hard.java
+++ src/be/ac/umons/stratego/ia/Ia_Hard.java
@@ -17,39 +17,71 @@ public class Ia_Hard {
                 if (plateau.board[i][j] != null && plateau.board[i][j].getThisriverpiece()==null ) {
                     Cell thiscell = plateau.board[i][j];
                     Pion thispawn = thiscell.getThispiece();
-                    Pion ennemy = spiraleAnalyse(thispawn, plateau);
-                    if (thispawn.getSquad().equals("Ennemy") && ennemy!=null) {
+                    Pion ennemy;
+                    if (thispawn.getSquad().equals("Ennemy") && (ennemy=spiraleAnalyse(thispawn, plateau))!=null) {
                         analyseBestChoice(thispawn, ennemy, plateau);
                         ///si un déplacement n'a pas été réalisé, on continue
                         if (plateau.board[i][j] == null)
-                            return;
+                            return; // arrete play car l'action executée
                     }
                 }
             }
         }
 
-        //ici on fera un déplacement aléatoire
+        //ici on fera un déplacement aléatoire si aucun ennemi est detecter ou aucune solution n'est possible
         Ia_easy.play(plateau);
     }
 
     public Pion spiraleAnalyse(Pion pion, PlateauBase plateau){
         int posX_Init = pion.getPosX();
         int posY_Init = pion.getPosY();
+         // 2 boucles de la spirale
         int i = 1;
-        while ( i<3) { // 3 boucles
-            if (analyseLigne(Direction.EAST,posX_Init,posY_Init,i,plateau) != null) // regarde pour chaque ligne de la spirale si pas ennemi ( Friend car moi )
-                return analyseLigne(Direction.EAST,posX_Init,posY_Init,i,plateau);// retourne l'ennemi ou null si pas
-            posX_Init += i;
-            if (analyseLigne(Direction.NORTH,posX_Init,posY_Init,i,plateau) != null)
-                return analyseLigne(Direction.NORTH,posX_Init,posY_Init,i,plateau);
-            posY_Init += i;
-            if (analyseLigne(Direction.WEST,posX_Init,posY_Init,i+1,plateau)!= null)
-                return analyseLigne(Direction.WEST,posX_Init,posY_Init,i+1,plateau);
-            posX_Init -= i+1;
-            if (analyseLigne(Direction.SOUTH,posX_Init,posY_Init,i+1,plateau)!= null)
-                return analyseLigne(Direction.SOUTH,posX_Init,posY_Init,i+1,plateau);
-            posY_Init -= i+1;
-            i++;
+        if (analyseLigne(Direction.EAST,posX_Init,posY_Init,i,plateau) != null) {  // regarde pour chaque ligne de la spirale si pas ennemi ( Friend car moi )
+            return analyseLigne(Direction.EAST, posX_Init, posY_Init, i, plateau);// retourne l'ennemi ou null si pas
+        }
+        posX_Init += 1;
+
+        if (analyseLigne(Direction.NORTH,posX_Init,posY_Init,i,plateau) != null) {
+            return analyseLigne(Direction.NORTH, posX_Init, posY_Init, i, plateau);
+        }
+        posY_Init += 1;
+        i++;
+
+        if (analyseLigne(Direction.WEST,posX_Init,posY_Init,i+1,plateau)!= null) {
+            return analyseLigne(Direction.WEST, posX_Init, posY_Init, i + 1, plateau);
+        }
+        posX_Init -= 2;
+
+        if (analyseLigne(Direction.SOUTH,posX_Init,posY_Init,i+1,plateau)!= null) {
+            return analyseLigne(Direction.SOUTH, posX_Init, posY_Init, i + 1, plateau);
+        }
+        posY_Init -= 2;
+        i++;
+
+        if (analyseLigne(Direction.EAST,posX_Init,posY_Init,i,plateau) != null) {
+            return analyseLigne(Direction.EAST, posX_Init, posY_Init, i, plateau);
+        }
+        posX_Init += 3;
+
+        if (analyseLigne(Direction.NORTH,posX_Init,posY_Init,i,plateau) != null) {
+            return analyseLigne(Direction.NORTH, posX_Init, posY_Init, i, plateau);
+        }
+        posY_Init += 3;
+        i++;
+
+        if (analyseLigne(Direction.WEST,posX_Init,posY_Init,i+1,plateau)!= null) {
+            return analyseLigne(Direction.WEST, posX_Init, posY_Init, i + 1, plateau);
+        }
+        posX_Init -= 4;
+
+        if (analyseLigne(Direction.SOUTH,posX_Init,posY_Init,i+1,plateau)!= null) {
+            return analyseLigne(Direction.SOUTH, posX_Init, posY_Init, i + 1, plateau);
+        }
+        posY_Init -= 4;
+
+        if (analyseLigne(Direction.EAST,posX_Init,posY_Init,i,plateau) != null) {
+            return analyseLigne(Direction.EAST, posX_Init, posY_Init, i, plateau);
         }
         return null;
     }
@@ -62,7 +94,7 @@ public class Ia_Hard {
             Pion p;
             if (x>=0 && x<10 && y>=0 && y<10 &&
                     (c=plateau.board[y][x])!=null &&
-                    (p=c.getThispiece())!=null && p.equals("Friend"))
+                    (p = c.getThispiece())!=null && p.equals("Friend"))
                 return plateau.board[posY_Init][posX_Init].getThispiece();
         }
         return null;
