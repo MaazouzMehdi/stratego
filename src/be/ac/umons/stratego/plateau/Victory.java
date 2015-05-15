d899a152dd23590f50b4c711a9a9d1d3f5584598
Marco <150938@student.umons.ac.be>
2015-05-15 02:10:15 +0200

    ton code c'était de la merde

--- src/be/ac/umons/stratego/plateau/Victory.java
+++ src/be/ac/umons/stratego/plateau/Victory.java
@@ -15,28 +15,23 @@ import java.util.ArrayList;
 public class Victory {
 
     public static boolean SomeoneWin(PlateauBase plateau){
-       return ( ( FlagDisponible("Friend",plateau) && MyPawnsCanMoves(plateau) ) && (FlagDisponible("Ennemy",plateau) && ComputerPawnsCanMoves(plateau) ) );
+        return !( ( FlagDisponible("Friend",plateau) && MyPawnsCanMoves(plateau) ) && (FlagDisponible("Ennemy",plateau) && ComputerPawnsCanMoves(plateau) ) );
+        // regarde si dans chaque équipe il manque un drapeau et si au moins un pion peut se déplacer
+        // dans le cas contraire , return true
     }
 
     public static boolean YouWin (PlateauBase plateau) {
-        if ( ( FlagDisponible("Friend",plateau) && MyPawnsCanMoves(plateau) ) && !(FlagDisponible("Ennemy",plateau) || ComputerPawnsCanMoves(plateau) ) )
-            return true;
-        else
-            return false;
+        return ( FlagDisponible("Friend",plateau) && MyPawnsCanMoves(plateau) ) && !(FlagDisponible("Ennemy",plateau) || ComputerPawnsCanMoves(plateau) );
+        // appelé que si SomeoneWin et return true si on gagne , sinon return false ( ce qui veut dire que l'ia a gagné
     }
-
 
     public static boolean FlagDisponible ( String squad,PlateauBase plateau){
         if (squad.equals("friend")){
              for (int i = 8;i<10;i++) {// regarde si le drapeau est toujours présent
                  for (int j=0;j<10;j++) {
                      Cell cell = plateau.board[i][j];
-                     if (cell==null )
-                         continue;
-                     else {
-                         if (cell.getThispiece().toString().equals("FLAG")) // on ne regarde pas l'équipe car le flag est toujours soit haut soit bas
+                     if (cell!=null  && cell.getThispiece().toString().equals("FLAG")) // on ne regarde pas l'équipe car le flag est toujours soit en haut soit en bas
                              return true;
-                     }
                  }
              }
             return false;
@@ -45,12 +40,8 @@ public class Victory {
             for (int i = 0;i<2;i++) {// regarde si le drapeau est toujours présent
                 for (int j=0;j<10;j++) {
                     Cell cell = plateau.board[i][j];
-                    if (cell==null )
-                        continue;
-                    else {
-                        if (cell.getThispiece().toString().equals("FLAG"))
-                            return true;
-                    }
+                    if (cell!=null && cell.getThispiece().toString().equals("FLAG"))
+                        return true;
                 }
             }
             return false;
@@ -62,15 +53,12 @@ public class Victory {
         for (int i=0;i<10;i++) {
             for (int j=0;j<10;j++) {
                 Cell cell = plateau.board[i][j];
-                if ((cell==null) ||  cell.getThisriverpiece()!=null ||(cell.getThispiece().getSquad().equals("Ennemy")))
-                	continue;
-                else {
-                    if (cell.getThispiece().deplacementPossible(Direction.NORTH, plateau, 1) ==true
-                            || cell.getThispiece().deplacementPossible(Direction.SOUTH, plateau, 1)==true
-                            || cell.getThispiece().deplacementPossible(Direction.EAST, plateau, 1)==true
-                            || cell.getThispiece().deplacementPossible(Direction.WEST, plateau, 1) == true );
+                if (!((cell==null) ||  cell.getThisriverpiece()!=null ||(cell.getThispiece().getSquad().equals("Ennemy"))) &&
+                        (cell.getThispiece().deplacementPossible(Direction.NORTH, plateau, 1)
+                            || cell.getThispiece().deplacementPossible(Direction.SOUTH, plateau, 1)
+                            || cell.getThispiece().deplacementPossible(Direction.EAST, plateau, 1)
+                            || cell.getThispiece().deplacementPossible(Direction.WEST, plateau, 1)))
                         return true;
-                }
             }
         }
         return false;
